26e6ea8e7450532ed31cb65c6d163ca5f5930628
Alex Tesevich <alext@didlogic.com>
2019-07-29 15:42:36 +0100

    init

---
+++ src/constants/Recipes.js
@@ -0,0 +1,8 @@
+export const RECIPES = [
+  {
+    id: 1,
+    title: 'Product 1',
+    price: 100,
+    imageUrl: 'https://www.royalcanin.com/vet/~/media/Royal-Canin/Product-Categories/cat-kitten-landing-hero.ashx',
+   },
+ ];
